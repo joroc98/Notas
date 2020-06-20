4fa90573cdc874373d16711922e636e656d4de0c
joroc98 <jorrocp1@teleco.upv.es>
2020-06-20 03:25:09 +0200

    menu

--- app/src/main/java/com/example/notas/MainActivity.java
+++ app/src/main/java/com/example/notas/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.notas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,6 +18,31 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     static ArrayList<String> notes =new ArrayList<>();
     static ArrayAdapter arrayAdapter;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+         super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.add_note) {
+
+            Intent intent = new Intent(getApplicationContext(), EditorNotas.class);
+
+            startActivity(intent);
+
+            return true;
+
+
+        }
+        return false;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
